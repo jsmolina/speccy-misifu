4cd2c838221939b14e14aa1b83c8d0d35aa9ba8f
Jordi Sesmero <jsmolina@gmail.com>
2018-09-02 19:56:35 +0200

    add info in sh script

--- makezx2.sh
+++ makezx2.sh
@@ -2,7 +2,8 @@
 # todo https://github.com/reidrac/png2sprite
 # todo http://www.mojontwins.com/mojoniaplus/viewtopic.php?t=1976
 # todo https://www.z88dk.org/wiki/doku.php?id=libnew:examples:sp1_ex1
-
+# todo https://www.z88dk.org/forum/viewtopic.php?id=10277
+# png2sp1sprite ./sprites/cat_sprites.png -i sprite_protar -f 32 > protar.asm
 zcc +zx -vn -m -startup=31 -clib=sdcc_iy juegozx2.c protar.asm -o juegozx2 -create-app
 
 rm -f juegozx2.bin
