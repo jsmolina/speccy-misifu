5c4f7912e969bd374393f76c6f7115a44ec78927
Jordi Sesmero <jsmolina@gmail.com>
2018-09-05 00:15:43 +0200

    show udg usage in comment

--- makezx2.sh
+++ makezx2.sh
@@ -5,6 +5,7 @@
 # todo https://www.z88dk.org/forum/viewtopic.php?id=10277
 # png2sp1sprite ./sprites/cat_sprites.png -i sprite_protar -f 32 > protar.asm
 #png2sp1sprite ./sprites/cat_sprites.png -i sprite_protar -f 32 > protar.asm
+# png2udg ./background/cubotop1.png
 zcc +zx -vn -m -startup=31 -clib=sdcc_iy juegozx2.c protar.asm -o juegozx2 -create-app
 
 rm -f juegozx2.bin
